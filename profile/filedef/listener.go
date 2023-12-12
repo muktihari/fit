5a311cc1410e5cd75c22795a0c06e325dd6771db
Hari Mukti <muktihaz@gmail.com>
2023-12-12 12:47:38 +0700

    feat!: filedef listener now support multiple common file types (#44)
    - now listener can accept multiple common file types during decoding chained FIT file
    - update usage.md documentation related to common file type

--- profile/filedef/listener.go
+++ profile/filedef/listener.go
@@ -5,32 +5,79 @@
 package filedef
 
 import (
+	"github.com/muktihari/fit/kit/typeconv"
+	"github.com/muktihari/fit/profile/typedef"
+	"github.com/muktihari/fit/profile/untyped/fieldnum"
+	"github.com/muktihari/fit/profile/untyped/mesgnum"
 	"github.com/muktihari/fit/proto"
 )
 
+type options struct {
+	channelBuffer uint
+	fileSets      FileSets
+}
+
+func defaultOptions() *options {
+	return &options{
+		channelBuffer: 1000,
+		fileSets:      PredefinedFileSet(),
+	}
+}
+
+// PredefinedFileSet is a list of default filesets used in listener, it's exported so user can append their own types and register it as an option.
+func PredefinedFileSet() FileSets {
+	return FileSets{
+		typedef.FileActivity: func() File { return NewActivity() },
+		typedef.FileCourse:   func() File { return NewCourse() },
+		typedef.FileWorkout:  func() File { return NewWorkout() },
+	}
+}
+
+type Option interface{ apply(o *options) }
+
+type fnApply func(o *options)
+
+func (f fnApply) apply(o *options) { f(o) }
+
+// WithChannelBuffer sets the size of buffered channel, default is 1000.
+func WithChannelBuffer(size uint) Option {
+	return fnApply(func(o *options) {
+		if size > 0 {
+			o.channelBuffer = size
+		}
+	})
+}
+
+// FileSets is a set of file type mapped to a function to create that File.
+type FileSets = map[typedef.File]func() File
+
+// WithFileSets sets what kind of file listener should listen to, when we encounter a file type that is not listed in fileset,
+// that file type will be skipped. This will replace the default filesets registered in listener, if you intend to append your own
+// file types, please call PredefinedFileSet() and add your file types.
+func WithFileSets(fileSets FileSets) Option {
+	return fnApply(func(o *options) {
+		if o.fileSets != nil {
+			o.fileSets = fileSets
+		}
+	})
+}
+
 // Listener is Message Listener.
-type Listener[F any, T FilePtr[F]] struct {
-	file    F
-	options *options
-	mesgc   chan proto.Message
-	done    chan struct{}
+type Listener struct {
+	options    *options
+	activeFile File
+	mesgc      chan proto.Message
+	done       chan struct{}
 }
 
-// FilePtr is a type constraint for pointer of File.
-type FilePtr[T any] interface {
-	*T
-	File
-}
-
-// NewListener creates mesg listener for given file T.
-func NewListener[F any, T FilePtr[F]](opts ...Option) *Listener[F, T] {
+// NewListener creates mesg listener.
+func NewListener(opts ...Option) *Listener {
 	options := defaultOptions()
 	for _, opt := range opts {
 		opt.apply(options)
 	}
 
-	l := &Listener[F, T]{
-		file:    *new(F),
+	l := &Listener{
 		options: options,
 		mesgc:   make(chan proto.Message, options.channelBuffer),
 		done:    make(chan struct{}),
@@ -41,60 +88,48 @@ func NewListener[F any, T FilePtr[F]](opts ...Option) *Listener[F, T] {
 	return l
 }
 
-func (l *Listener[F, T]) loop() {
+func (l *Listener) loop() {
 	for mesg := range l.mesgc {
-		T(&l.file).Add(mesg)
+		if mesg.Num == mesgnum.FileId {
+			fileType := typeconv.ToByte[typedef.File](mesg.FieldValueByNum(fieldnum.FileIdType))
+			newFile, ok := l.options.fileSets[fileType]
+			if !ok {
+				continue
+			}
+			l.activeFile = newFile()
+		}
+		if l.activeFile == nil {
+			continue // No file is created since not defined in fileSets. Skip.
+		}
+		l.activeFile.Add(mesg)
 	}
 	close(l.done)
 }
 
-func (l *Listener[F, T]) OnMesg(mesg proto.Message) { l.mesgc <- mesg }
+func (l *Listener) OnMesg(mesg proto.Message) { l.mesgc <- mesg }
 
 // Close closes channel and wait until all messages is consumed.
-func (l *Listener[F, T]) Close() {
+func (l *Listener) Close() {
 	close(l.mesgc)
 	<-l.done
 }
 
-// File returns the resulting file after the a single decode process is completed. This will reset fields used by listener
+// File returns the resulting file after the a single decode process is completed. If we the current decoded result is not listed
+// in fileSets, nil will be returned, it's  recommended to use switch type assertion to check. This will reset fields used by listener
 // and the listener is ready to be used for next chained FIT file.
-func (l *Listener[F, T]) File() T {
+func (l *Listener) File() File {
 	l.Close()
 
-	file := l.file
+	file := l.activeFile
 	l.reset()
 
 	go l.loop()
 
-	return T(&file)
+	return file
 }
 
-func (l *Listener[F, T]) reset() {
-	l.file = *new(F)
+func (l *Listener) reset() {
+	l.activeFile = nil
 	l.mesgc = make(chan proto.Message, l.options.channelBuffer)
 	l.done = make(chan struct{})
-}
-
-type options struct {
-	channelBuffer uint
-}
-
-func defaultOptions() *options {
-	return &options{
-		channelBuffer: 1000,
-	}
-}
-
-type Option interface{ apply(o *options) }
-
-type fnApply func(o *options)
-
-func (f fnApply) apply(o *options) { f(o) }
-
-func WithChannelBuffer(size uint) Option {
-	return fnApply(func(o *options) {
-		if size > 0 {
-			o.channelBuffer = size
-		}
-	})
 }
