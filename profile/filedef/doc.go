0b893d37a31c5c142d7d155ee4bd3f72b31340b3
Hari Mukti <muktihaz@gmail.com>
2023-12-28 23:56:01 +0700

    chore: add missing copyright header (#76)

--- profile/filedef/doc.go
+++ profile/filedef/doc.go
@@ -1,3 +1,7 @@
+// Copyright 2023 The Fit SDK for Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package filedef contains the implementation of known common file types and its listener
 // to convert decoded FIT file into the desired common file type as soon as the message is decoded.
 package filedef
