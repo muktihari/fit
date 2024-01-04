20e031bf9db388564be42624792a991e36798921
Hikmatulloh Hari Mukti <muktihaz@gmail.com>
2024-01-04 15:58:36 +0700

    perf: add benchmark NewRecord and Record.ToMesg()

---
+++ profile/mesgdef/record_gen_test.go
@@ -0,0 +1,27 @@
+package mesgdef
+
+import (
+	"testing"
+
+	"github.com/muktihari/fit/factory"
+	"github.com/muktihari/fit/profile/untyped/mesgnum"
+)
+
+func BenchmarkNewRecord(b *testing.B) {
+	mesg := factory.CreateMesg(mesgnum.Record)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = NewRecord(&mesg)
+	}
+}
+
+func BenchmarkRecordToMesg(b *testing.B) {
+	mesg := factory.CreateMesg(mesgnum.Record)
+	record := NewRecord(&mesg)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = record.ToMesg(factory.StandardFactory())
+	}
+}
