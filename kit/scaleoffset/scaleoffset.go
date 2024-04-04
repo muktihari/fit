a9187bdf114c272b16d4b243339fcc0ae9d9fc88
Hikmatulloh Hari Mukti <muktihaz@gmail.com>
2024-04-04 11:34:31 +0700

    scaleoffset add ApplyValue and DiscardValue func

--- kit/scaleoffset/scaleoffset.go
+++ kit/scaleoffset/scaleoffset.go
@@ -28,11 +28,11 @@ func ApplySlice[S []E, E Numeric](values S, scale, offset float64) []float64 {
 	return vals
 }
 
-// ApplyValue applies scale and offset when possible, otherwise return original value.
-// This function can only accept primitive-types such as int8, []int8, uint32, []uint32, etc in the form of proto.Value.
-func ApplyValue(value proto.Value, scale, offset float64) proto.Value {
-	if scale == 1 && offset == 0 {
-		return value
+// ApplyValue applies scale and offset when possible and return it as primitive-types value. Otherwise return original value.
+func ApplyValue(val any, scale, offset float64) proto.Value {
+	value, ok := val.(proto.Value)
+	if !ok {
+		return proto.Any(ApplyAny(val, scale, offset))
 	}
 	switch value.Type() {
 	case proto.TypeInt8:
@@ -79,6 +79,60 @@ func ApplyValue(value proto.Value, scale, offset float64) proto.Value {
 	return value // not supported, return original value
 }
 
+// ApplyAny applies scale and offset when possible and return it as primitive-types value. Otherwise return original value.
+func ApplyAny(value any, scale, offset float64) any {
+	if scale == 1 && offset == 0 {
+		return value
+	}
+
+	switch val := value.(type) {
+	case proto.Value:
+		return ApplyValue(val, scale, offset).Any()
+	case int8:
+		return Apply(val, scale, offset)
+	case []int8:
+		return ApplySlice(val, scale, offset)
+	case uint8:
+		return Apply(val, scale, offset)
+	case []uint8:
+		return ApplySlice(val, scale, offset)
+	case int16:
+		return Apply(val, scale, offset)
+	case []int16:
+		return ApplySlice(val, scale, offset)
+	case uint16:
+		return Apply(val, scale, offset)
+	case []uint16:
+		return ApplySlice(val, scale, offset)
+	case int32:
+		return Apply(val, scale, offset)
+	case []int32:
+		return ApplySlice(val, scale, offset)
+	case uint32:
+		return Apply(val, scale, offset)
+	case []uint32:
+		return ApplySlice(val, scale, offset)
+	case int64:
+		return Apply(val, scale, offset)
+	case []int64:
+		return ApplySlice(val, scale, offset)
+	case uint64:
+		return Apply(val, scale, offset)
+	case []uint64:
+		return ApplySlice(val, scale, offset)
+	case float32:
+		return Apply(val, scale, offset)
+	case []float32:
+		return ApplySlice(val, scale, offset)
+	case float64:
+		return Apply(val, scale, offset)
+	case []float64:
+		return ApplySlice(val, scale, offset)
+	default:
+		return val // not supported, return original value
+	}
+}
+
 // Discard discards applied scale and offset on value.
 func Discard(value, scale, offset float64) float64 {
 	if scale == 1 && offset == 0 {
@@ -100,7 +154,12 @@ func DiscardSlice[T Numeric](values []float64, scale, offset float64) []T {
 	return vals
 }
 
-func DiscardValue(value proto.Value, baseType basetype.BaseType, scale, offset float64) proto.Value {
+// DiscardValue restores scaled value in the form of float64 or []float64 to its basetype's form.
+func DiscardValue(val any, baseType basetype.BaseType, scale, offset float64) proto.Value {
+	value, ok := val.(proto.Value)
+	if !ok {
+		return proto.Any(DiscardAny(val, baseType, scale, offset))
+	}
 	switch value.Type() {
 	case proto.TypeFloat64:
 		dv := Discard(value.Float64(), scale, offset)
@@ -151,4 +210,61 @@ func DiscardValue(value proto.Value, baseType basetype.BaseType, scale, offset f
 		}
 	}
 	return value
+}
+
+// DiscardAny restores scaled value in the form of float64 or []float64 to its basetype's form and return it as primitive-types value.
+func DiscardAny(value any, baseType basetype.BaseType, scale, offset float64) any {
+	switch val := value.(type) {
+	case proto.Value:
+		return DiscardValue(val, baseType, scale, offset).Any()
+	case float64: // a scaled value will always in float64 form.
+		dv := Discard(val, scale, offset)
+		switch baseType {
+		case basetype.Sint8:
+			return int8(dv)
+		case basetype.Byte, basetype.Uint8, basetype.Uint8z:
+			return uint8(dv)
+		case basetype.Sint16:
+			return int16(dv)
+		case basetype.Uint16, basetype.Uint16z:
+			return uint16(dv)
+		case basetype.Sint32:
+			return int32(dv)
+		case basetype.Uint32, basetype.Uint32z:
+			return uint32(dv)
+		case basetype.Float32:
+			return float32(dv)
+		case basetype.Float64:
+			return float64(dv)
+		case basetype.Sint64:
+			return int64(dv)
+		case basetype.Uint64, basetype.Uint64z:
+			return uint64(dv)
+		}
+	case []float64: // array of scaled values will always in []float64 form.
+		switch baseType {
+		case basetype.Byte, basetype.Uint8, basetype.Uint8z:
+			return DiscardSlice[uint8](val, scale, offset)
+		case basetype.Sint8:
+			return DiscardSlice[int8](val, scale, offset)
+		case basetype.Sint16:
+			return DiscardSlice[int16](val, scale, offset)
+		case basetype.Uint16, basetype.Uint16z:
+			return DiscardSlice[uint16](val, scale, offset)
+		case basetype.Sint32:
+			return DiscardSlice[int32](val, scale, offset)
+		case basetype.Uint32, basetype.Uint32z:
+			return DiscardSlice[uint32](val, scale, offset)
+		case basetype.Float32:
+			return DiscardSlice[float32](val, scale, offset)
+		case basetype.Float64:
+			return DiscardSlice[float64](val, scale, offset)
+		case basetype.Sint64:
+			return DiscardSlice[int64](val, scale, offset)
+		case basetype.Uint64, basetype.Uint64z:
+			return DiscardSlice[uint64](val, scale, offset)
+		}
+	}
+
+	return value // not supported, return original value
 }
--- kit/scaleoffset/scaleoffset_test.go
+++ kit/scaleoffset/scaleoffset_test.go
@@ -16,7 +16,7 @@ import (
 
 func TestApplyValue(t *testing.T) {
 	tt := []struct {
-		value  proto.Value
+		value  any
 		scale  float64
 		offset float64
 		result proto.Value
@@ -46,10 +46,16 @@ func TestApplyValue(t *testing.T) {
 		{value: proto.SliceInt8([]int8{10}), scale: 5, offset: 1, result: proto.SliceFloat64([]float64{1})},
 		{value: proto.String("fit"), scale: 1, offset: 0, result: proto.String("fit")},
 		{value: proto.String("fit"), scale: 255, offset: 255, result: proto.String("fit")},
+		{value: uint16(37304), scale: 5, offset: 500, result: proto.Float64(6960.8)},
 	}
 
 	for i, tc := range tt {
-		t.Run(fmt.Sprintf("[%d] %#v", i, tc.value.Any()), func(t *testing.T) {
+		name := fmt.Sprintf("[%d] %#v", i, tc.value)
+		value, ok := tc.value.(proto.Value)
+		if ok {
+			name = fmt.Sprintf("[%d] %#v", i, value.Any())
+		}
+		t.Run(name, func(t *testing.T) {
 			result := scaleoffset.ApplyValue(tc.value, tc.scale, tc.offset)
 			if diff := cmp.Diff(result, tc.result,
 				cmp.Transformer("Value", func(v proto.Value) any {
@@ -62,10 +68,60 @@ func TestApplyValue(t *testing.T) {
 	}
 }
 
+func TestApplyAny(t *testing.T) {
+	tt := []struct {
+		value  any
+		scale  float64
+		offset float64
+		result any
+	}{
+		{value: uint16(37304), scale: 5, offset: 500, result: float64(6960.8)},
+		{value: int8(10), scale: 5, offset: 1, result: float64(1)},
+		{value: uint8(10), scale: 5, offset: 1, result: float64(1)},
+		{value: int16(10), scale: 5, offset: 1, result: float64(1)},
+		{value: uint16(10), scale: 5, offset: 1, result: float64(1)},
+		{value: int32(10), scale: 5, offset: 1, result: float64(1)},
+		{value: uint32(10), scale: 5, offset: 1, result: float64(1)},
+		{value: int64(10), scale: 5, offset: 1, result: float64(1)},
+		{value: uint64(10), scale: 5, offset: 1, result: float64(1)},
+		{value: float32(10), scale: 5, offset: 1, result: float64(1)},
+		{value: float64(10), scale: 5, offset: 1, result: float64(1)},
+		{value: []uint16{37304}, scale: 5, offset: 500, result: []float64{6960.8}},
+		{value: []int8{10}, scale: 5, offset: 1, result: []float64{1}},
+		{value: []uint8{10}, scale: 5, offset: 1, result: []float64{1}},
+		{value: []int16{10}, scale: 5, offset: 1, result: []float64{1}},
+		{value: []uint16{10}, scale: 5, offset: 1, result: []float64{1}},
+		{value: []int32{10}, scale: 5, offset: 1, result: []float64{1}},
+		{value: []uint32{10}, scale: 5, offset: 1, result: []float64{1}},
+		{value: []int64{10}, scale: 5, offset: 1, result: []float64{1}},
+		{value: []uint64{10}, scale: 5, offset: 1, result: []float64{1}},
+		{value: []float32{10}, scale: 5, offset: 1, result: []float64{1}},
+		{value: []float64{10}, scale: 5, offset: 1, result: []float64{1}},
+		{value: []int8{10}, scale: 5, offset: 1, result: []float64{1}},
+		{value: string("fit"), scale: 1, offset: 0, result: "fit"},
+		{value: string("fit"), scale: 255, offset: 255, result: "fit"},
+		{value: proto.Uint8(10), scale: 5, offset: 1, result: float64(1)},
+	}
+
+	for i, tc := range tt {
+		name := fmt.Sprintf("[%d] %#v", i, tc.value)
+		value, ok := tc.value.(proto.Value)
+		if ok {
+			name = fmt.Sprintf("[%d] %#v", i, value.Any())
+		}
+		t.Run(name, func(t *testing.T) {
+			result := scaleoffset.ApplyAny(tc.value, tc.scale, tc.offset)
+			if diff := cmp.Diff(result, tc.result); diff != "" {
+				t.Fatal(diff)
+			}
+		})
+	}
+}
+
 func TestDiscardValue(t *testing.T) {
 	tt := []struct {
 		name     string
-		value    proto.Value
+		value    any
 		scale    float64
 		offset   float64
 		baseType basetype.BaseType
@@ -104,16 +160,82 @@ func TestDiscardValue(t *testing.T) {
 		{value: proto.SliceFloat64([]float64{6960.8}), scale: 5, offset: 500, baseType: basetype.Float64, result: proto.SliceFloat64([]float64{37304})},
 		{value: proto.SliceFloat64([]float64{6960.8}), scale: 1, offset: 0, baseType: basetype.Float64, result: proto.SliceFloat64([]float64{6960.8})},
 		{value: proto.String("fit"), scale: 5, offset: 500, baseType: basetype.Float64, result: proto.String("fit")},
+		{value: float64(6960.8), scale: 5, offset: 500, baseType: basetype.Uint16z, result: proto.Uint16(37304)},
 	}
 
 	for i, tc := range tt {
-		t.Run(fmt.Sprintf("[%d]%#v", i, tc.value.Any()), func(t *testing.T) {
+		name := fmt.Sprintf("[%d] %#v", i, tc.value)
+		value, ok := tc.value.(proto.Value)
+		if ok {
+			name = fmt.Sprintf("[%d] %#v", i, value.Any())
+		}
+		t.Run(name, func(t *testing.T) {
 			result := scaleoffset.DiscardValue(tc.value, tc.baseType, tc.scale, tc.offset)
 			if diff := cmp.Diff(result, tc.result,
 				cmp.Transformer("Value", func(v proto.Value) any {
 					return v.Any()
 				}),
 			); diff != "" {
+				t.Fatal(diff)
+			}
+		})
+	}
+}
+
+func TestDiscardAny(t *testing.T) {
+	tt := []struct {
+		name     string
+		value    any
+		scale    float64
+		offset   float64
+		baseType basetype.BaseType
+		result   any
+	}{
+		{value: float64(6960.8), scale: 5, offset: 500, baseType: basetype.Uint16, result: uint16(37304)},
+		{value: float64(5), scale: 2, offset: 2, baseType: basetype.Byte, result: byte(14)},
+		{value: float64(5), scale: 2, offset: 2, baseType: basetype.Sint8, result: int8(14)},
+		{value: float64(5), scale: 2, offset: 2, baseType: basetype.Uint8, result: uint8(14)},
+		{value: float64(5), scale: 2, offset: 2, baseType: basetype.Uint8z, result: uint8(14)},
+		{value: float64(5), scale: 2, offset: 2, baseType: basetype.Sint16, result: int16(14)},
+		{value: float64(6960.8), scale: 5, offset: 500, baseType: basetype.Uint16z, result: uint16(37304)},
+		{value: float64(6960.8), scale: 5, offset: 500, baseType: basetype.Sint32, result: int32(37304)},
+		{value: float64(6960.8), scale: 5, offset: 500, baseType: basetype.Uint32, result: uint32(37304)},
+		{value: float64(6960.8), scale: 5, offset: 500, baseType: basetype.Uint32z, result: uint32(37304)},
+		{value: float64(6960.8), scale: 5, offset: 500, baseType: basetype.Sint64, result: int64(37304)},
+		{value: float64(6960.8), scale: 5, offset: 500, baseType: basetype.Uint64, result: uint64(37304)},
+		{value: float64(6960.8), scale: 5, offset: 500, baseType: basetype.Uint64z, result: uint64(37304)},
+		{value: float64(6960.8), scale: 5, offset: 500, baseType: basetype.Float32, result: float32(37304)},
+		{value: float64(6960.8), scale: 5, offset: 500, baseType: basetype.Float64, result: float64(37304)},
+		{value: float64(6960.8), scale: 1, offset: 0, baseType: basetype.Float64, result: float64(6960.8)},
+		{value: []float64{6960.8}, scale: 5, offset: 500, baseType: basetype.Uint16, result: []uint16{37304}},
+		{value: []float64{5}, scale: 2, offset: 2, baseType: basetype.Byte, result: []byte{14}},
+		{value: []float64{5}, scale: 2, offset: 2, baseType: basetype.Sint8, result: []int8{14}},
+		{value: []float64{5}, scale: 2, offset: 2, baseType: basetype.Uint8, result: []uint8{14}},
+		{value: []float64{5}, scale: 2, offset: 2, baseType: basetype.Uint8z, result: []uint8{14}},
+		{value: []float64{5}, scale: 2, offset: 2, baseType: basetype.Sint16, result: []int16{14}},
+		{value: []float64{6960.8}, scale: 5, offset: 500, baseType: basetype.Uint16z, result: []uint16{37304}},
+		{value: []float64{6960.8}, scale: 5, offset: 500, baseType: basetype.Sint32, result: []int32{37304}},
+		{value: []float64{6960.8}, scale: 5, offset: 500, baseType: basetype.Uint32, result: []uint32{37304}},
+		{value: []float64{6960.8}, scale: 5, offset: 500, baseType: basetype.Uint32z, result: []uint32{37304}},
+		{value: []float64{6960.8}, scale: 5, offset: 500, baseType: basetype.Sint64, result: []int64{37304}},
+		{value: []float64{6960.8}, scale: 5, offset: 500, baseType: basetype.Uint64, result: []uint64{37304}},
+		{value: []float64{6960.8}, scale: 5, offset: 500, baseType: basetype.Uint64z, result: []uint64{37304}},
+		{value: []float64{6960.8}, scale: 5, offset: 500, baseType: basetype.Float32, result: []float32{37304}},
+		{value: []float64{6960.8}, scale: 5, offset: 500, baseType: basetype.Float64, result: []float64{37304}},
+		{value: []float64{6960.8}, scale: 1, offset: 0, baseType: basetype.Float64, result: []float64{6960.8}},
+		{value: string("fit"), scale: 5, offset: 500, baseType: basetype.Float64, result: string("fit")},
+		{value: proto.Float64(6960.8), scale: 5, offset: 500, baseType: basetype.Sint64, result: int64(37304)},
+	}
+
+	for i, tc := range tt {
+		name := fmt.Sprintf("[%d] %#v", i, tc.value)
+		value, ok := tc.value.(proto.Value)
+		if ok {
+			name = fmt.Sprintf("[%d] %#v", i, value.Any())
+		}
+		t.Run(name, func(t *testing.T) {
+			result := scaleoffset.DiscardAny(tc.value, tc.baseType, tc.scale, tc.offset)
+			if diff := cmp.Diff(result, tc.result); diff != "" {
 				t.Fatal(diff)
 			}
 		})
