eded39eddda56e8b89d001ddd11af107e89879de
Hikmatulloh Hari Mukti <muktihaz@gmail.com>
2024-04-06 14:31:24 +0700

    fix scaleoffset.ApplyValue to not allocate

--- kit/scaleoffset/scaleoffset.go
+++ kit/scaleoffset/scaleoffset.go
@@ -29,11 +29,7 @@ func ApplySlice[S []E, E Numeric](values S, scale, offset float64) []float64 {
 }
 
 // ApplyValue applies scale and offset when possible and return it as primitive-types value. Otherwise return original value.
-func ApplyValue(val any, scale, offset float64) proto.Value {
-	value, ok := val.(proto.Value)
-	if !ok {
-		return proto.Any(ApplyAny(val, scale, offset))
-	}
+func ApplyValue(value proto.Value, scale, offset float64) proto.Value {
 	switch value.Type() {
 	case proto.TypeInt8:
 		return proto.Float64(Apply(value.Int8(), scale, offset))
