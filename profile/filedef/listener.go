2b8ae7df524ea3ef0e5aa5fac782f210ee3b286d
Hikmatulloh Hari Mukti <muktihaz@gmail.com>
2024-05-02 18:46:38 +0700

    fix: listener must clone DeveloperFields

--- profile/filedef/listener.go
+++ profile/filedef/listener.go
@@ -9,6 +9,7 @@ import (
 	"github.com/muktihari/fit/profile/untyped/fieldnum"
 	"github.com/muktihari/fit/profile/untyped/mesgnum"
 	"github.com/muktihari/fit/proto"
+	"golang.org/x/exp/slices"
 )
 
 // Listener is Message Listener.
@@ -144,16 +145,8 @@ func (l *Listener) prep(mesg proto.Message) proto.Message {
 	copy(m.Fields, mesg.Fields)
 	mesg.Fields = m.Fields
 
-	if mesg.DeveloperFields == nil {
-		return mesg
-	}
-
-	if cap(m.DeveloperFields) < len(mesg.DeveloperFields) {
-		m.DeveloperFields = make([]proto.DeveloperField, len(mesg.DeveloperFields))
-	}
-	m.DeveloperFields = m.DeveloperFields[:len(mesg.DeveloperFields)]
-	copy(m.DeveloperFields, mesg.DeveloperFields)
-	mesg.DeveloperFields = m.DeveloperFields
+	// Must clone DeveloperFields since it is being referenced in mesgdef's structs.
+	mesg.DeveloperFields = slices.Clone(mesg.DeveloperFields)
 
 	return mesg
 }
