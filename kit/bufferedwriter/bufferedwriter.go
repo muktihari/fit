0f9d45b032b650d565444ed89f5e4f5cf62e2524
Hari Mukti <muktihaz@gmail.com>
2024-05-14 12:32:00 +0700

    chore: clean up kit's packages and add doc.go (#264)
    * chore: clean up kit's packages and add doc.go
    
    * chore: no need to check size, let bufio.NewWriterSize decides

--- kit/bufferedwriter/bufferedwriter.go
+++ kit/bufferedwriter/bufferedwriter.go
@@ -9,94 +9,71 @@ import (
 	"io"
 )
 
-const (
-	defaultBufferSize = 4 << 10 // 4096 KB
-)
+const defaultBufferSize = 4 << 10 // 4096 KB
 
-// BufferedWriter is a writter with buffer, it supports io.Writer, io.WriterAt and io.WriterSeeker.
+// BufferedWriter is a writter with buffer, it supports io.Writer, io.WriterAt and io.WriteSeeker.
 type BufferedWriter interface {
 	io.Writer
 	// Flush writes any buffered data to the underlying io.Writer.
 	Flush() error
 }
 
-// New is shorthand for NewSize(w, 4096), a 4KB Buffer.
+// New is shorthand for NewSize(w, 4096), a 4KB Buffer. See NewSize() for details.
 func New(w io.Writer) BufferedWriter {
 	return NewSize(w, defaultBufferSize)
 }
 
-// NewSize creates a buffered writer with a specified size while taking into account the underlying capabilities of the writer w,
-// which might implement either [io.WriterAt] or [io.WriteSeeker]. This allows the buffered writer to maintain the ability to write at a specific byte position.
+// NewSize creates a buffered writer with a specified size while taking into account the underlying
+// capabilities of the writer w, which might implement either [io.WriterAt] or [io.WriteSeeker].
+// This allows the buffered writer to maintain the ability to write at a specific byte position.
 //
 // Use-case scenario:
-//   - An *os.File may be passed to a function receiving an [io.Writer], while that function may assert the original ability of the value to write at a specific byte
-//     position if possible to enable a faster processing path. Nonetheless, directly working with *os.File for frequent writing small byte segments can affect
-//     performance due to numerous syscalls. To alleviate this issue, incorporating a buffered writer for the *os.File becomes essential.
-//     Unlike bufio.Writer that encapsulates everything as a buffered [io.Writer], this approach preserves the inherent capabilities of [io.WriterAt] and [io.WriteSeeker].
+//   - An *os.File may be passed to a function receiving an [io.Writer], while that function may
+//     assert the original ability of the value to write at a specific byte position if possible to
+//     enable a faster processing path. Nonetheless, directly working with *os.File for frequent writing
+//     small byte segments can affect performance due to numerous syscalls. To alleviate this issue,
+//     incorporating a buffered writer for the *os.File becomes essential. Unlike bufio.Writer that
+//     encapsulates everything as an [io.Writer], this approach preserves the inherent
+//     capabilities of [io.WriterAt] and [io.WriteSeeker].
 //
-// Just like any other buffered writer, the Flush() method should be called after the process is completed to write the unwritten buffered data.
+// Just like any other buffered writer, the Flush() method should be called after the process is
+// completed to write the unwritten buffered data.
 func NewSize(w io.Writer, size int) BufferedWriter {
-	if size < defaultBufferSize {
-		size = defaultBufferSize
+	bw := bufio.NewWriterSize(w, size)
+	switch wr := w.(type) {
+	case io.WriterAt:
+		return &WriterAt{bw, wr}
+	case io.WriteSeeker:
+		return &WriteSeeker{bw, wr}
+	default:
+		return bw
 	}
-
-	if wra, ok := w.(io.WriterAt); ok {
-		return &WriterAt{
-			bw:  bufio.NewWriterSize(w, size),
-			wra: wra,
-		}
-	}
-
-	if wrs, ok := w.(io.WriteSeeker); ok {
-		return &WriteSeeker{
-			bw:  bufio.NewWriterSize(w, size),
-			wrs: wrs,
-		}
-	}
-
-	return bufio.NewWriterSize(w, size)
 }
 
+// WriterAt is an io.WriterAt buffer wrapper.
 type WriterAt struct {
-	bw  *bufio.Writer
-	wra io.WriterAt
-}
-
-func (w *WriterAt) Write(p []byte) (n int, err error) {
-	return w.bw.Write(p)
+	*bufio.Writer
+	wa io.WriterAt
 }
 
 func (w *WriterAt) WriteAt(p []byte, offset int64) (n int, err error) {
-	if err = w.bw.Flush(); err != nil { // flush any buffered data from buffered writer in case we are writing on unflushed data.
+	// Flush any buffered data from buffered writer in case we are writing on unflushed data.
+	if err = w.Writer.Flush(); err != nil {
 		return
 	}
-	w.bw.Reset(w.wra.(io.Writer)) // reset buffer
-
-	return w.wra.WriteAt(p, offset)
+	return w.wa.WriteAt(p, offset)
 }
 
-func (w *WriterAt) Flush() error { return w.bw.Flush() }
-
+// WriteSeeker is an io.WriteSeeker buffer wrapper.
 type WriteSeeker struct {
-	bw  *bufio.Writer
-	wrs io.WriteSeeker
-}
-
-func (w *WriteSeeker) Write(p []byte) (n int, err error) {
-	return w.bw.Write(p)
+	*bufio.Writer
+	sk io.Seeker
 }
 
 func (w *WriteSeeker) Seek(offset int64, whence int) (n int64, err error) {
-	if err = w.bw.Flush(); err != nil { // flush any buffered data from buffered writer in case we are seeking on unflushed data.
+	// Flush any buffered data from buffered writer in case we are seeking on unflushed data.
+	if err = w.Writer.Flush(); err != nil {
 		return
 	}
-	w.bw.Reset(w.wrs) // reset buffer
-
-	n, err = w.wrs.Seek(offset, whence)
-	if err != nil {
-		return
-	}
-	return
+	return w.sk.Seek(offset, whence)
 }
-
-func (w *WriteSeeker) Flush() error { return w.bw.Flush() }
