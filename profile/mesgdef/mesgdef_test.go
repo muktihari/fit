fe8fd6c5289a8fb43b7851bf8b66d4dfd61c7ed6
Mukti <muktihaz@gmail.com>
2024-08-31 17:10:22 +0700

    chore: add missing copyright header (#386)

--- profile/mesgdef/mesgdef_test.go
+++ profile/mesgdef/mesgdef_test.go
@@ -1,3 +1,7 @@
+// Copyright 2024 The FIT SDK for Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package mesgdef
 
 import (
