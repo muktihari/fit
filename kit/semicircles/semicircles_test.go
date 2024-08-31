be543d0e67fff9f0ab75867a22e8f1d0a1c1fe9c
Mukti <muktihaz@gmail.com>
2024-08-31 23:56:03 +0700

    feat: semicircles now handles invalid value (#388)

--- kit/semicircles/semicircles_test.go
+++ kit/semicircles/semicircles_test.go
@@ -6,9 +6,11 @@ package semicircles_test
 
 import (
 	"fmt"
+	"math"
 	"testing"
 
 	"github.com/muktihari/fit/kit/semicircles"
+	"github.com/muktihari/fit/profile/basetype"
 )
 
 func TestToDegrees(t *testing.T) {
@@ -18,11 +20,12 @@ func TestToDegrees(t *testing.T) {
 	}{
 		{semicircles: -91900524, degrees: -7.703012935817242},
 		{semicircles: 1319585919, degrees: 110.6064139958471},
+		{semicircles: basetype.Sint32Invalid, degrees: math.Float64frombits(basetype.Float64Invalid)},
 	}
 	for _, tc := range tt {
 		t.Run(fmt.Sprintf("%#v", tc.semicircles), func(t *testing.T) {
 			result := semicircles.ToDegrees(tc.semicircles)
-			if result != tc.degrees {
+			if math.Float64bits(result) != math.Float64bits(tc.degrees) {
 				t.Fatalf("expected: %v, got: %v", tc.degrees, result)
 			}
 		})
@@ -36,6 +39,9 @@ func TestToSemicircles(t *testing.T) {
 	}{
 		{degrees: -7.703012935817242, semicircles: -91900524},
 		{degrees: 110.6064139958471, semicircles: 1319585919},
+		{degrees: math.Float64frombits(basetype.Float64Invalid), semicircles: basetype.Sint32Invalid},
+		{degrees: math.NaN(), semicircles: basetype.Sint32Invalid},
+		{degrees: math.Inf(0), semicircles: basetype.Sint32Invalid},
 	}
 	for _, tc := range tt {
 		t.Run(fmt.Sprintf("%#v", tc.degrees), func(t *testing.T) {
