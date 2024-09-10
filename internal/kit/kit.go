e1c5036efa2cdde1171a3476594322efd8f869b0
Hikmatulloh Hari Mukti <muktihaz@gmail.com>
2024-09-11 00:26:35 +0700

    remove internal/kit

--- internal/kit/kit.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2023 The FIT SDK for Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-package kit
-
-// Ptr returns new pointer of v
-func Ptr[T any](v T) *T { return &v }
