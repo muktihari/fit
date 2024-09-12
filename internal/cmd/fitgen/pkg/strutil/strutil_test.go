95656d61cfa3edcb4aa501869fbcd3a631b7434c
Hikmatulloh Hari Mukti <muktihaz@gmail.com>
2024-09-12 16:06:26 +0700

    add unit test for strutil.ToTitle

--- internal/cmd/fitgen/pkg/strutil/strutil_test.go
+++ internal/cmd/fitgen/pkg/strutil/strutil_test.go
@@ -5,10 +5,46 @@
 package strutil_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/muktihari/fit/internal/cmd/fitgen/pkg/strutil"
 )
+
+func BenchmarkToTitle(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = strutil.ToTitle("avg_speed")
+	}
+}
+
+func TestToTitle(t *testing.T) {
+	tt := []struct {
+		input  string
+		output string
+	}{
+		{
+			input:  "avg_speed",
+			output: "AvgSpeed",
+		},
+		{
+			input:  "avg speed",
+			output: "AvgSpeed",
+		},
+		{
+			input:  "timestamp_32k",
+			output: "Timestamp32K",
+		},
+	}
+
+	for i, tc := range tt {
+		t.Run(fmt.Sprintf("[%d] %s", i, tc.input), func(t *testing.T) {
+			out := strutil.ToTitle(tc.input)
+			if out != tc.output {
+				t.Fatalf("expected: %q, got: %q", tc.output, out)
+			}
+		})
+	}
+}
 
 func TestTrimRepeatedChar(t *testing.T) {
 	tt := []struct {
